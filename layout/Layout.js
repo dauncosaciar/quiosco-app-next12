96ba0dc8dbc4c9fc20ecf10dd045727b9d8709ca
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-02 14:34:31 -0300

    showing product on modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,25 @@
 import Head from "next/head";
+import Modal from "react-modal";
 import Sidebar from "../components/Sidebar";
+import ModalProducto from "../components/ModalProducto";
+import useQuiosco from "../hooks/useQuioso";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useQuiosco();
+
   return (
     <>
       <Head>
@@ -18,6 +36,12 @@ export default function Layout({ children, pagina }) {
           <div className="p-10">{children}</div>
         </main>
       </div>
+
+      {modal && (
+        <Modal isOpen={modal} style={customStyles}>
+          <ModalProducto />
+        </Modal>
+      )}
     </>
   );
 }
