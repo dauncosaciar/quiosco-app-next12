1b32132297f597468a078065318651b60bf4a1fc
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-02 11:10:33 -0300

    getting current product category

--- context/QuioscoProvider.js
+++ context/QuioscoProvider.js
@@ -5,6 +5,7 @@ const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState([]);
+  const [categoriaActual, setCategoriaActual] = useState({});
 
   const obtenerCategorias = async () => {
     const { data } = await axios("/api/categorias");
@@ -15,10 +16,21 @@ const QuioscoProvider = ({ children }) => {
     obtenerCategorias();
   }, []);
 
+  useEffect(() => {
+    setCategoriaActual(categorias[0]);
+  }, [categorias]);
+
+  const handleClickCategoria = id => {
+    const categoria = categorias.filter(cat => cat.id === id);
+    setCategoriaActual(categoria[0]);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
-        categorias
+        categorias,
+        categoriaActual,
+        handleClickCategoria
       }}
     >
       {children}
