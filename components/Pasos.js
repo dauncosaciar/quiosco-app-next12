6725cd0a0ec65e01193bdd7ef7288f2291a2d79c
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-04 10:45:13 -0300

    finishing progress bar

--- components/Pasos.js
+++ components/Pasos.js
@@ -9,7 +9,19 @@ const pasos = [
 const Pasos = () => {
   const router = useRouter();
 
-  console.log("router:", router);
+  const calcularProgreso = () => {
+    let valor;
+
+    if (router.pathname === "/") {
+      valor = 2;
+    } else if (router.pathname === "/resumen") {
+      valor = 50;
+    } else {
+      valor = 100;
+    }
+
+    return valor;
+  };
 
   return (
     <>
@@ -26,6 +38,13 @@ const Pasos = () => {
             {paso.nombre}
           </button>
         ))}
+      </div>
+
+      <div className="bg-gray-100 mb-10">
+        <div
+          className="rounded-full bg-amber-500 text-xs leading-none h-2 text-center text-white"
+          style={{ width: `${calcularProgreso()}%` }}
+        ></div>
       </div>
     </>
   );
