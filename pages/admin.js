6d28e6a69c1cf79dd131c91e1f71d7b9a175bc6d
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-07 22:31:13 -0300

    showing orders in admin page

--- pages/admin.js
+++ pages/admin.js
@@ -1,10 +1,27 @@
+import useSWR from "swr";
+import axios from "axios";
 import AdminLayout from "../layout/AdminLayout";
+import Orden from "../components/Orden";
 
 export default function Admin() {
+  const fetcher = () => axios("/api/ordenes").then(datos => datos.data);
+
+  const { data, error, isLoading } = useSWR("/api/ordenes", fetcher);
+
+  // console.log("data:", data);
+  // console.log("error:", error);
+  // console.log("isLoading:", isLoading);
+
   return (
     <AdminLayout pagina="Admin">
       <h1 className="text-4xl font-black">Panel de Administración</h1>
       <p className="text-2xl my-10">Administra las órdenes</p>
+
+      {data && data.length ? (
+        data.map(orden => <Orden key={orden.id} orden={orden} />)
+      ) : (
+        <p>No hay órdenes pendientes</p>
+      )}
     </AdminLayout>
   );
 }
